7c00af989bea0cd8c4dd3e6b3feda63edeaf3111
Chaorong Chen <ccrjohn8787@gmail.com>
2013-07-17 03:58:33 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var response_str = buf.toString(fs.readFileSync('index.html'));
+  response.send(response_str);
 });
 
 var port = process.env.PORT || 5000;
