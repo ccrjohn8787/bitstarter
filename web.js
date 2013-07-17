1b68c1d469caa8c1b4ba2d627e1e006641e14f58
Chaorong Chen <ccrjohn8787@gmail.com>
2013-07-17 04:21:38 +0000

    again

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var response_str = buf.toString(fs.readFileSync('index.html'));
+  fs.readFileSync('index.html');
+  var response_str = buf.toString();
   response.send(response_str);
 });
 
